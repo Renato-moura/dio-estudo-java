1e51cd9219f9bd73cc8ba3d1f03443bab4d89716
Renato-moura <renatomoura.ms@gmail.com>
2024-05-26 21:07:20 -0300

    Initial commit

---
+++ desafio-restfull/src/main/java/edu/desafio/restfull/desafio_restfull/DesafioRestfullApplication.java
@@ -0,0 +1,13 @@
+package edu.desafio.restfull.desafio_restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioRestfullApplication.class, args);
+	}
+
+}
---
+++ desafio-restfull/src/test/java/edu/desafio/restfull/desafio_restfull/DesafioRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.desafio.restfull.desafio_restfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
